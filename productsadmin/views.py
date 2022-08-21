11adac98c5d692272a1d34023bdb6e40c9260b5c
Thomas Forselius <forselius.thomas@gmail.com>
2022-08-21 21:09:36 +0200

    fixed update function in views.py and form population in edit_prod.html

--- productsadmin/views.py
+++ productsadmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -22,5 +22,22 @@ def add_prod(request):
         return redirect('show_prod')
     return render(request, 'admin/add_prod.html')
 
+
 def edit_prod(request, prod_id):
-    return render(request, 'productsadmin/edit_prod.html')
+    prod = get_object_or_404(Product, id=prod_id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        desc = request.POST.get("desc")
+        sale = 'sale' in request.POST
+        sale_price = request.POST.get("sale_price")
+        Product.objects.filter(pk=prod_id).update(name=name, price=price, desc=desc, sale=sale, sale_price=sale_price)
+        return redirect('show_prod')
+    context = {
+        'name' : prod.name,
+        'price' : prod.price,
+        'desc' : prod.desc,
+        'sale' : prod.sale,
+        'sale_price' : prod.sale_price
+    }
+    return render(request, 'admin/edit_prod.html', context)
