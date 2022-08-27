90722b4dcc49cbf1e5c2eb032d2b104e53e69b4e
Thomas Forselius <forselius.thomas@gmail.com>
2022-08-27 09:19:20 +0200

    deleted forms.py

--- productsadmin/forms.py
+++
@@ -1,9 +0,0 @@
-from socket import fromshare
-from django import forms
-from .models import Product
-
-class ProdForm(forms.ModelForm):
-    class Meta: 
-        model = Product
-        fields = ['name', 'price', 'desc', 'sale', 'sale_price']
-        
--- productsadmin/templates/forms.py
+++
@@ -1,9 +0,0 @@
-from socket import fromshare
-from django import forms
-from .models import Item
-
-class ProdForm(forms.ProductForm):
-    class Meta: 
-        model = Item
-        fields = ['name', 'price', 'desc', 'sale', 'sale_price']
-        
