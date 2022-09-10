6c1c31fa3416186aab05f4e9bcccbcbe35429100
Thomas Forselius <forselius.thomas@gmail.com>
2022-09-10 23:04:08 +0200

    changed DEBUG to true

--- pickyproducts/settings.py
+++ pickyproducts/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9*=+e^pg*a75^3wf7b+*4%$_zt*n=fb7_2#&e(y*z$540+w*m#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pickyproducts.herokuapp.com/']
 
