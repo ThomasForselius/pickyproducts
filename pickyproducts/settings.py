d1e50b9bc7421c3b384b99eea71577d77d6cf27d
Thomas Forselius <forselius.thomas@gmail.com>
2022-09-29 18:51:19 +0200

    removed secret key from settings

--- pickyproducts/settings.py
+++ pickyproducts/settings.py
@@ -32,10 +32,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-9*=+e^pg*a75^3wf7b+*4%$_zt*n=fb7_2#&e(y*z$540+w*m#')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 #ALLOWED_HOSTS = ['localhost']
