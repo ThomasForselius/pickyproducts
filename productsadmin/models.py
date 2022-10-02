185d853d12d8d98e1f7cba7eb41c9f59be72a643
Thomas Forselius <forselius.thomas@gmail.com>
2022-10-02 21:33:54 +0200

    added likes to Product model

--- productsadmin/models.py
+++ productsadmin/models.py
@@ -1,4 +1,7 @@
+from tkinter import CASCADE
 from django.db import models
+
+from macpath import realpath
 
 # Create your database models here.
 
@@ -9,6 +12,10 @@ class Product(models.Model):
     pic = models.CharField(max_length=30, null=False, blank=False, default='pic.jpg')
     sale = models.BooleanField(null=False, blank=False, default=False)
     sale_price = models.DecimalField(null=False, blank=False, max_digits=5, decimal_places=2)
+    likes = models.ManyToManyField(User, related_name="likes", blank=True)
 
     def __str__(self):
         return self.name # displays the item name in the Admin panel instead of 'Item /number'
+
+    def num_of_likes(self):
+        return self.likes.count()
